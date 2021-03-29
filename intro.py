0d2b85a6ac4fa0f3b4ea9a05a15c44bbd1bab988
dojo24 <mlongenberger@codingdojo.com>
2021-03-29 01:37:12 -0400

    added base files

---
+++ intro.py
@@ -0,0 +1,10 @@
+# http://learn.codingdojo.com/m/208/5813/39347
+
+
+# 1. Print the following string: Welcome to Python
+
+
+# 2. Create a variable and add it to a string using a , and then +
+
+
+# 3. Create variables and add them into a string using f-String, string.format(), %-formating, and built in (.title())
