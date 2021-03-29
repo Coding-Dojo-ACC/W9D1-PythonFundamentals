cec95f9cc5e6fce36faa42f8c83cb5d4b7d7c67b
dojo24 <mlongenberger@codingdojo.com>
2021-03-29 01:54:47 -0400

    ready for class

--- intro.py
+++ intro.py
@@ -1,10 +1,14 @@
 # http://learn.codingdojo.com/m/208/5813/39347
 
 
-# 1. Print the following string: Welcome to Python
+# 1. Print the following string: Welcome to Python!
 
 
 # 2. Create a variable and add it to a string using a , and then +
 
 
-# 3. Create variables and add them into a string using f-String, string.format(), %-formating, and built in (.title())
+# 3. Create variables and add them into a string using f-String, string.format(), and %-formating
+
+
+#  Create a variables and change the way the print using different string methods
+
