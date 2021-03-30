2180e3a0e612b9d1938f6eb8a8fa4217bf3b0bad
dojo24 <mlongenberger@codingdojo.com>
2021-03-29 21:17:48 -0400

    saivng all changes

--- intro.py
+++ intro.py
@@ -1,13 +1,39 @@
 # http://learn.codingdojo.com/m/208/5813/39347
 
+# print("Hello World!")
+var1 = "What's Cooking Houston"
+var2 = 42
+
+# print(var1)
+# print(var2)
+
 
 # 1. Print the following string: Welcome to Python!
-
+# print("Welcome to Python!")
 
 # 2. Create a variable and add it to a string using a , and then +
+holiday = "Easter"
+print(holiday, "is comming")
+
+rain = "showers"
+print("April " + rain + " brings May Flowers")
+
+print(holiday, "is comming and April " + rain + " brings May flowers")
 
 
 # 3. Create variables and add them into a string using f-String, string.format(), and %-formating
+firstName = "Melissa"
+age = 42
+location = "Berwick, PA"
+
+# f-string
+print(f"My name is {firstName}, I live in {location} and I am {age} years old")
+
+# string.format()
+print("I live in {} and I am {} years old, my name is {}".format(location, age, firstName))
+
+#  %-formating
+print("I am %s years old.  My name is %s and I live in %s" % (age, firstName, location))
 
 
 #  Create a variables and change the way the print using different string methods
